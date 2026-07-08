8b7a9dd8f3b02248afba702f64468650851721ac
Rks54321 <rakeshkumar756131@gmail.com>
2026-07-08 23:19:29 +0530

    Add Two Sum Solution

---
+++ src/arrays/TwoSum.java
@@ -0,0 +1,24 @@
+package arrays;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+public class TwoSum {
+    static void main(String[] args) {
+        int[] nums = {1,2,3,4,5,6};
+        int target = 9;
+
+        int[] result = twoSum(nums, target);
+        System.out.println(Arrays.toString(result));
+    }
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0 ; i  < nums.length; i++) {
+            int complement = target - nums[i];
+            if(map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{-1,-1};
+    }
+}

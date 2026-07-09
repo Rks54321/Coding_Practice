0cfba9e9d0ef58d4f9b4dfb724d06f8dd19a08e1
Rks54321 <rakeshkumar756131@gmail.com>
2026-07-09 21:51:39 +0530

    Add Container with most water

---
+++ src/arrays/ContainerWithMostWater.java
@@ -0,0 +1,38 @@
+package arrays;
+
+public class ContainerWithMostWater {
+
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int maxWater = 0;
+
+        while (left < right) {
+
+            int minHeight = Math.min(height[left], height[right]);
+            int width = right - left;
+            int area = minHeight * width;
+
+            maxWater = Math.max(maxWater, area);
+
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxWater;
+    }
+
+    public static void main(String[] args) {
+
+        ContainerWithMostWater obj = new ContainerWithMostWater();
+
+        int[] height = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+
+        int result = obj.maxArea(height);
+
+        System.out.println("Maximum Water = " + result);
+    }
+}

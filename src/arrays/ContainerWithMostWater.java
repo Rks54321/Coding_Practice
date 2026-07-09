ba4c0bef6eecc5ede522782bad9ce34ae8f398c6
Rks54321 <rakeshkumar756131@gmail.com>
2026-07-09 21:53:33 +0530

    Add Container with most water2

--- src/arrays/ContainerWithMostWater.java
+++ src/arrays/ContainerWithMostWater.java
@@ -2,29 +2,46 @@ package arrays;
 
 public class ContainerWithMostWater {
 
+//    public int maxArea(int[] height) {
+//        int left = 0;
+//        int right = height.length - 1;
+//        int maxWater = 0;
+//
+//        while (left < right) {
+//
+//            int minHeight = Math.min(height[left], height[right]);
+//            int width = right - left;
+//            int area = minHeight * width;
+//
+//            maxWater = Math.max(maxWater, area);
+//
+//            if (height[left] < height[right]) {
+//                left++;
+//            } else {
+//                right--;
+//            }
+//        }
+//
+//        return maxWater;
+//    }
+
+    // Brute Force Approach
     public int maxArea(int[] height) {
-        int left = 0;
-        int right = height.length - 1;
         int maxWater = 0;
 
-        while (left < right) {
+        for (int i = 0; i < height.length; i++) {
+            for (int j = i + 1; j < height.length; j++) {
 
-            int minHeight = Math.min(height[left], height[right]);
-            int width = right - left;
-            int area = minHeight * width;
+                int minHeight = Math.min(height[i], height[j]);
+                int width = j - i;
+                int area = minHeight * width;
 
-            maxWater = Math.max(maxWater, area);
-
-            if (height[left] < height[right]) {
-                left++;
-            } else {
-                right--;
+                maxWater = Math.max(maxWater, area);
             }
         }
 
         return maxWater;
     }
-
     public static void main(String[] args) {
 
         ContainerWithMostWater obj = new ContainerWithMostWater();

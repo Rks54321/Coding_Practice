959d3ae796646fc6032285b20257dadd001eeec6
Rks54321 <rakeshkumar756131@gmail.com>
2026-07-12 17:00:44 +0530

    Add the sorting array

--- src/CircularLinkedList/InsertCircularLinkedList2.java
+++ src/CircularLinkedList/InsertCircularLinkedList2.java
@@ -1,5 +1,26 @@
 package CircularLinkedList;
-
+import java.util.Scanner;
+class Nodee{
+    int val;
+    Nodee next;
+    Nodee(int val) {
+        this.val = val;
+    }
+}
 public class InsertCircularLinkedList2 {
-    mai
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String line = sc.nextLine().trim();
+        int[] arr;
+        if(line.isEmpty()) {
+            arr = new int[0];
+        } else {
+            String[] parts = line.split(",");
+            arr = new int[parts.length];
+            for(int i = 0; i < parts.length; i++) {
+                arr[i] = Integer.parseInt(parts[i].trim());
+            }
+        }
+        int x = Integer.parseInt(sc.nextLine().trim());
+    }
 }
---
+++ src/arrays/SortingArray.java
@@ -0,0 +1,19 @@
+package arrays;
+import java.util.Arrays;
+public class SortingArray {
+    static void main(String[] args) {
+        int[] arr = {5,3,6,7,2,1};
+        for(int i = 0 ; i < arr.length - 1; i++) {
+            for(int j = 0 ; j < arr.length - 1 - i; j++) {
+                if(arr[j] > arr[j+1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        for(int x : arr) {
+            System.out.print(x + " ");
+        }
+    }
+}

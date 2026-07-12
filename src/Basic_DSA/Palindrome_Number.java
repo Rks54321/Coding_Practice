4852f660da4d4ccbf2a7dd0098ee8d96747cf46d
Rks54321 <rakeshkumar756131@gmail.com>
2026-07-12 17:46:29 +0530

    Palindrome Nummber

---
+++ src/Basic_DSA/Palindrome_Number.java
@@ -0,0 +1,19 @@
+package Basic_DSA;
+
+public class Palindrome_Number {
+    static void main(String[] args) {
+        int num = 121;
+        int original = num;
+        int reverse = 0;
+        while(num > 0) {
+            int digit = num % 10;
+            reverse = reverse *  10 + digit;
+            num /= 10;
+        }
+        if(reverse == original) {
+            System.out.println("Palindrome Number");
+        } else {
+            System.out.println("Not Palindrome Number");
+        }
+    }
+}

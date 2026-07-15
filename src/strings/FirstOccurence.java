2d23717435887a52d66e18ff2007f64908ab0a6c
Rks54321 <rakeshkumar756131@gmail.com>
2026-07-15 09:31:45 +0530

    First Occurence

---
+++ src/strings/FirstOccurence.java
@@ -0,0 +1,24 @@
+package strings;
+
+public class FirstOccurence {
+    static void main(String[] args) {
+        String haystack = "butsad";
+        String needle = "sad";
+        System.out.println(firstOccurence(haystack, needle));
+    }
+    public static int firstOccurence(String haystack, String needle) {
+        int m = haystack.length();
+        int n = needle.length();
+
+        if(n == 0) return 0;
+
+        for(int i = 0; i <= m - n; i++) {
+            int j = 0;
+            while(j < n && haystack.charAt(i + j) == needle.charAt(j)) {
+                j++;
+            }
+            if(j == n) return i;
+        }
+        return -1;
+    }
+}

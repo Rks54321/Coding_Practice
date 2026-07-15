b4281d6d588381f21a9c30c63be3ace7ce1b7010
Rks54321 <rakeshkumar756131@gmail.com>
2026-07-15 08:49:04 +0530

    Coin Change dp practice

---
+++ src/dp/CoinChange2.java
@@ -0,0 +1,22 @@
+package dp;
+
+public class CoinChange2 {
+    static void main(String[] args) {
+        int[] coins = {1,2,5};
+        int amount = 11;
+        System.out.println(coinChange(coins, amount));
+    }
+    public static int coinChange(int[] coins, int amount) {
+        if(amount < 1) return 0;
+        int[] dp = new int[amount+1];
+        for(int i = 1; i <= amount; i++) {
+            dp[i] = Integer.MAX_VALUE;
+            for(int coin : coins) {
+                if(coin <= i && dp[i-coin] != Integer.MAX_VALUE) {
+                    dp[i] = Math.min(dp[i], dp[i-coin]+1);
+                }
+            }
+        }
+        return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
+    }
+}

5b46916d89d49f4b1e0d569c391cf48cc0873268
Rks54321 <rakeshkumar756131@gmail.com>
2026-07-15 09:53:21 +0530

    Group anagram

---
+++ src/strings/GroupAnagram2.java
@@ -0,0 +1,29 @@
+package strings;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Arrays;
+import java.util.ArrayList;
+//imporrt java
+public class GroupAnagram2 {
+    static void main(String[] args) {
+        String[] strs = {"eat","tea","tan","ate","nat","bat"};
+        System.out.println(groupAnagram(strs));
+    }
+    public static List<List<String>> groupAnagram(String[] strs)   {
+        Map<String, List<String>> map = new HashMap<>();
+        for(String s : strs) {
+            char[] arr = s.toCharArray();
+            Arrays.sort(arr);
+            String key = new String(arr);
+            if(map.containsKey(key)) {
+                map.get(key).add(s);
+            } else {
+                List<String> list = new ArrayList<>();
+                list.add(s);
+                map.put(key, list);
+            }
+        }
+        return new ArrayList<>(map.values());
+    }
+}

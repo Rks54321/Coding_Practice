96ccb22f37117b685fc4c553d100f5b2d4d0b683
Rks54321 <rakeshkumar756131@gmail.com>
2026-07-15 08:33:08 +0530

    Valid Parenthesis practice

---
+++ src/strings/validPalindrome2.java
@@ -0,0 +1,19 @@
+package strings;
+
+public class validPalindrome2 {
+    static void main(String[] args) {
+        String s = "raca a car";
+        System.out.println(validPalindrome(s));
+    }
+    public static boolean validPalindrome(String s) {
+        int i = 0, j = s.length() - 1;
+        while( i < j) {
+            while(i < j && !Character.isLetterOrDigit(s.charAt(i))) i++;
+            while(i < j && !Character.isLetterOrDigit(s.charAt(j))) j--;
+            if(Character.toLowerCase(s.charAt(i++)) != Character.toLowerCase(s.charAt(j--))) {
+                return false;
+            }
+        }
+        return true;
+    }
+}

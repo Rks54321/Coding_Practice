ccfb1bc66c5f86b8dbecff31008f3e1144c24ef1
Rks54321 <rakeshkumar756131@gmail.com>
2026-07-27 16:56:12 +0530

    Product of all elemenets except self

---
+++ src/arrays/ProdOfArrayExceptSelf.java
@@ -0,0 +1,28 @@
+package arrays;
+import java.util.Arrays;
+public class ProdOfArrayExceptSelf {
+    static void main(String[] args) {
+        int[] arr = {1,2,3,4};
+        int[] result = prodOfArrExSelf(arr);
+        System.out.println(Arrays.toString(result));
+    }
+    public static int[] prodOfArrExSelf(int[] arr) {
+        int[] left = new int[arr.length];
+        int[] right = new int[arr.length];
+
+        left[0] = 1;
+        right[arr.length-1] = 1;
+
+        for(int i = 1; i < arr.length; i++) {
+            left[i] = arr[i-1] * left[i-1];
+        }
+        for(int i = arr.length-2; i >= 0; i--) {
+            right[i] = arr[i+1] * right[i+1];
+        }
+        int[] result = new int[arr.length];
+        for(int i = 0; i < arr.length; i++) {
+            result[i] = left[i] * right[i];
+        }
+        return result;
+    }
+}

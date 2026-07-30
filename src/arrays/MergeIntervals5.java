991c0b59210b6eabed99d9fe48128229a11c63ac
Rks54321 <rakeshkumar756131@gmail.com>
2026-07-30 12:40:41 +0530

    Merge intervals Practice

---
+++ src/arrays/MergeIntervals5.java
@@ -0,0 +1,38 @@
+package arrays;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+public class MergeIntervals5 {
+    static void main(String[] args) {
+        int[][] intervals = {
+                {1,3},
+                {2,8},
+                {10,13},
+                {14,18}
+        };
+        int[][] result = mergeIntervals(intervals);
+        for(int i = 0; i < result.length; i++) {
+            System.out.print(Arrays.toString(result[i]));
+            if(i != result.length-1) {
+                System.out.print(",");
+            }
+        }
+//        System.out.print(Arrays.deepToString(result));
+    }
+    public static int[][] mergeIntervals(int[][] intervals) {
+        if(intervals.length <= 1) return intervals;
+        Arrays.sort(intervals, (a,b) -> Integer.compare(a[0], b[0]));
+        List<int[]> result = new ArrayList<>();
+        int[] curr = intervals[0];
+        for(int i = 1; i < intervals.length; i++) {
+            if(curr[1] > intervals[i][0]) {
+                curr[1] = Math.max(curr[1], intervals[i][1]);
+            } else {
+                result.add(curr);
+                curr = intervals[i];
+            }
+        }
+        result.add(curr);
+        return result.toArray(new int[result.size()][]);
+    }
+}

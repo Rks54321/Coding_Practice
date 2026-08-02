1d2875f3b4844b60b49ba6234179fd4dd6cdb6c8
Rks54321 <rakeshkumar756131@gmail.com>
2026-08-02 12:34:56 +0530

    aximum Length Substring with Two Occurences

---
+++ src/Kodnest/MaxLenSubStrWithTwoOccr.java
@@ -0,0 +1,21 @@
+package Kodnest;
+import java.util.Scanner;
+public class MaxLenSubStrWithTwoOccr {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        int[] freq = new int[256];
+        int left = 0, maxLen = 0;
+        for(int right = 0; right < s.length(); right++) {
+            char ch = s.charAt(right);
+            freq[ch]++;
+            while(freq[ch] > 2) {
+                freq[s.charAt(left)]--;
+                left++;
+            }
+            maxLen = Math.max(maxLen, right-left+1);
+        }
+        System.out.println(maxLen);
+
+    }
+}

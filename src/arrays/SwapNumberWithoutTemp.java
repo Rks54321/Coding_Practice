debf82386fc75a7b4aafa6971318f7eb0e95757d
Rks54321 <rakeshkumar756131@gmail.com>
2026-08-04 22:33:09 +0530

    Swap Number without extra variable Practice

--- src/arrays/SwapNumberWithoutTemp.java
+++ src/arrays/SwapNumberWithoutTemp.java
@@ -8,7 +8,6 @@ public class SwapNumberWithoutTemp {
         a = a + b;
         b = a - b;
         a = a - b;
-
         System.out.println("a = " + a);
         System.out.println("b = " + b);
     }

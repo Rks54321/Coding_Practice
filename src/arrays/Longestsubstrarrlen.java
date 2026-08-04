9c01260db90d5286e82a9a64ad2dd271280e7c95
Rks54321 <rakeshkumar756131@gmail.com>
2026-08-04 22:31:16 +0530

    Longest SubString Length Practice

--- src/arrays/Longestsubstrarrlen.java
+++ src/arrays/Longestsubstrarrlen.java
@@ -17,6 +17,7 @@ public class Longestsubstrarrlen {
                     left++;
                 }
             }
+        
         return minLength == Integer.MAX_VALUE ? 0 : minLength;
     }
 }

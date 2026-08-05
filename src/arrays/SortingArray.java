a321cfa332dd494dd378229514c936161035211b
Rks54321 <rakeshkumar756131@gmail.com>
2026-08-05 11:59:59 +0530

    Two Sum Practice

--- src/arrays/SortingArray.java
+++ src/arrays/SortingArray.java
@@ -12,7 +12,7 @@ public class SortingArray {
                 }
             }
         }
-        
+
         System.out.println(Arrays.toString(arr));
 //        for(int x : arr) {
 //            System.out.print(x + " ");

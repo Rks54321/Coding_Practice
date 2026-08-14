8359eeb1e9c9344df6ec2cf2cc58a71f5ec4ce83
Rks54321 <rakeshkumar756131@gmail.com>
2026-08-14 22:41:22 +0530

    Finding the minimum maximum and average of array

---
+++ Python/NumPy/n2.py
@@ -0,0 +1,9 @@
+import numpy as np
+arr1 = np.array([1,2,3,4,5,6])
+arr2 = np.array([7,8,9,10,11,12])
+# Find max value in arr2
+print(arr2.max())
+# Find min value in arr1
+print(arr1.min())
+# Find average in arr2
+print(arr2.mean())

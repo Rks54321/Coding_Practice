096afdb64b55036344c5fc4a6be10b02dc4ee213
Rks54321 <rakeshkumar756131@gmail.com>
2026-08-27 22:42:21 +0530

    Product of array except self

--- src/arrays/ProdOfArrayExceptSelf.java
+++ src/arrays/ProdOfArrayExceptSelf.java
@@ -22,6 +22,7 @@ public class ProdOfArrayExceptSelf {
         for(int i = 0; i < arr.length; i++) {
             result[i] = left[i] * right[i];
         }
+        
         return result;
     }
 }

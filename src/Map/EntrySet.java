0f15044ed92bf1a7a99c578ba19e77553fe2fc5a
Rks54321 <rakeshkumar756131@gmail.com>
2026-08-27 22:44:16 +0530

    Entry set Practice

--- src/Map/EntrySet.java
+++ src/Map/EntrySet.java
@@ -8,6 +8,7 @@ public class EntrySet {
         map.put("banana", 200);
         for(Map.Entry<String, Integer> entry : map.entrySet()) {
             System.out.println(entry.getKey() + " " + entry.getValue());
+            
         }
     }
 }

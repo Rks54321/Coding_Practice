f01676cf90e3f3579a831d741c434b764ce7e183
Rks54321 <rakeshkumar756131@gmail.com>
2026-08-30 11:45:27 +0530

    Queue using Stack 2 Practice

---
+++ src/StackNQueue/QueueUsingStack2.java
@@ -0,0 +1,32 @@
+package StackNQueue;
+import java.util.Stack;
+public class QueueUsingStack2 {
+    Stack<Integer> stack1 = new Stack<>();
+    Stack<Integer> stack2 = new Stack<>();
+
+    void enqueue(int x) {
+        stack1.push(x);
+    }
+    int dequeue() {
+        if(stack2.isEmpty()) {
+            while(!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+        if(stack2.isEmpty()) {
+            System.out.println("Queue is Empty");
+            return -1;
+        }
+        return stack2.pop();
+    }
+    public static void main(String[] args) {
+        QueueUsingStack2 q = new QueueUsingStack2();
+        q.enqueue(10);
+        q.enqueue(20);
+        q.enqueue(30);
+
+        System.out.println(q.dequeue());
+        System.out.println(q.dequeue());
+        System.out.println(q.dequeue());
+    }
+}
